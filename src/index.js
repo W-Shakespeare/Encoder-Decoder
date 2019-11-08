acb7e6640003dcd111de7b898658082dd0dc87f3
W-Shakespeare <kiiiikkiiiik2@gmail.com>
2019-11-08 21:30:22 +0200

    Add @media

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./css/css.css";
 import "./css/worms.css";
-
+import "./css/cssMedia.css";
 import Text from "./text";
 import EncoderContainer from "./encoder/encoderContainer";
 import DecoderContainer from "./decoder/decoderContainer";
